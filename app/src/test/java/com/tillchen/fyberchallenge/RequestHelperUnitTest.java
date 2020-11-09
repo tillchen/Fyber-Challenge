22996a779322794ab3bf32c5aef072c2abff13d5
Tianyao Chen <tillchen417@gmail.com>
2020-11-09 02:01:00 +0100

    Fix unit tests

--- app/src/test/java/com/tillchen/fyberchallenge/RequestHelperUnitTest.java
+++ app/src/test/java/com/tillchen/fyberchallenge/RequestHelperUnitTest.java
@@ -14,13 +14,23 @@ import org.powermock.modules.junit4.PowerMockRunner;
 public class RequestHelperUnitTest {
 
     @Test
-    public void testHashStringWithSecurityToken() {
+    public void testHashParamsWithSecurityToken() {
         // The expected hash is obtained from http://www.sha1-online.com
         String expected = "2d43ca826012f8d8d2f430950fbd22a15704b2b1";
         String stringToHash = "appid=2070&ip=109.235.143.113&locale=DE&offer_types=112"
                 + "&timestamp=1604870998&uid=superman";
         String securityToken = "1c915e3b5d42d05136185030892fbb846c278927";
         String actual = RequestHelper.hashParamsWithSecurityToken(stringToHash, securityToken);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testHashResponseWithSecurityToken() {
+        // The expected hash is obtained from http://www.sha1-online.com
+        String expected = "ebf25d001c029edf0087df0e2b26215d44246c25";
+        String stringToHash = "foo";
+        String securityToken = "1c915e3b5d42d05136185030892fbb846c278927";
+        String actual = RequestHelper.hashResponseWithSecurityToken(stringToHash, securityToken);
         Assert.assertEquals(expected, actual);
     }
 
@@ -41,7 +51,7 @@ public class RequestHelperUnitTest {
     public void testIsResponseCorrupted() {
         String securityToken = "1c915e3b5d42d05136185030892fbb846c278927";
         String validSignature =
-                RequestHelper.hashParamsWithSecurityToken("foo", securityToken);
+                RequestHelper.hashResponseWithSecurityToken("foo", securityToken);
         Assert.assertFalse(RequestHelper.isResponseCorrupted(
                 "foo", securityToken, validSignature));
         Assert.assertTrue(RequestHelper.isResponseCorrupted(
